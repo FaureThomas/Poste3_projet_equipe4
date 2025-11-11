67eab06124beabe054dcfe78a637f45f0d2236c0
mateoferrage <186382602+mateoferrage@users.noreply.github.com>
2025-11-11 21:50:07 +0100

    Ajout de la V1.3

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -20,3 +20,14 @@ DATA="${DIR}info_meteo.txt"
 curl -s "wttr.in/${VILLE}?format=j2" -o "$DATA"
 #je vais chercher en ligne les données de la ville, je les mets en format json compact pour un meilleur traitement 
 #puis je les assignent à $DATA
+
+
+TEMP=$(grep -m1 '"temp_C"' "$DATA" | sed 's/[^0-9-]*//g')
+#je récupère la température actuelle en recherchant la première occurence de "temp_C"
+#puis le formatage en gardant les chiffre et le "-"
+
+TEMP_DEMAIN=$(grep '"avgtempC"' "$DATA" | sed 's/[^0-9-]*//g' | sed -n '2p')
+#je récupère la temp moyenne du lendemain en récupérant toutes les lignes contenant "avgtempC"
+#formatage en gardant que les chiffre encore une fois
+#le dernier pipe sert à ne garder que la deuxième valeur qui correspond au lebdemain
+
