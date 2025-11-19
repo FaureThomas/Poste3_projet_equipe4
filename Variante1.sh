312356cd95330c96ae2bb760455ad2f8a22571df
Girard Alexia <alexia.girard27@gmail.com>
2025-11-19 23:09:27 +0100

    Variante 1 finale

--- Variante1.sh
+++ Variante1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Pour la variante 1, on reprend le script des version, ici sans les commentaire pour une meilleure visibilitée
 if [ $# -eq 0 ]; then
     VILLE="Toulouse"
 else
@@ -11,6 +11,8 @@ DIR_SCRIPT="$(dirname "$0")"
 METEO="${DIR_SCRIPT}/meteo${DATE_METEOTXT}.txt"
 
 DATA="info_meteo.txt"
+
+#Le curl permet de prendre le format necessaire afin d'afficher vent, humidité et visibilité.
 curl -s --max-time 10 "wttr.in/${VILLE}?format=%t|%w|%h|%v&no-terminal" -o "$DATA"
 
 if [ ! -s "$DATA" ]; then
@@ -24,10 +26,10 @@ RAW=$(echo "$RAW" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
 
 IFS='|' read -r TEMP VENT HUMIDITE VISIBILITE <<< "$RAW"
 
-# Nettoyer TEMP (enlever +)
+
 TEMP="${TEMP#+}"
 
-# Nettoyer VENT (ne garder que les chiffres et km/h)
+# Nettoyer VENT (ne garder que les chiffres et km/h) afin d'éviter les problèmes d affichage
 VENT_CHIFFRES=$(echo "$VENT" | grep -o '[0-9]\+ *km/h')
 if [ -z "$VENT_CHIFFRES" ]; then
     VENT_CHIFFRES="N/A"
@@ -40,7 +42,6 @@ fi
 
 DATE=$(date +"%Y-%m-%d -%H:%M")
 
-# Exemple d'écriture
 echo "${DATE} -${VILLE} : ${TEMP}°C - Vent : ${VENT_CHIFFRES} - Humidite : ${HUMIDITE} - Visibilite : ${VISIBILITE}" >> "$METEO"
 
 rm "$DATA"
