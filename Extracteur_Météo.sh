94b284e2196ed95ac188ff8c8edd0eb3ad8c74ea
Girard Alexia <alexia.girard27@gmail.com>
2025-11-20 20:33:11 +0100

    Variante 1

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -54,3 +54,65 @@ echo "${DATE} -${VILLE} : ${TEMP}°C - ${TEMP_DEMAIN}°C" >> "meteo.txt"
 
 rm "$DATA"
 #On supprime le fichier temporaire car on en a plus besoin.
+
+
+#______________________________________________________ Variante 1:_______________________________________________________________
+
+#!/bin/bash
+#Pour la variante 1, on reprend le script des version, ici sans les commentaire pour une meilleure visibilitée
+if [ $# -eq 0 ]; then
+    VILLE="Toulouse"
+else
+    VILLE=$1
+fi
+
+DATE_METEOTXT=$(date +"%Y%m%d")
+DIR_SCRIPT="$(dirname "$0")"
+METEO="${DIR_SCRIPT}/meteo${DATE_METEOTXT}.txt"
+
+DATA="info_meteo.txt"
+
+#Le curl permet de prendre le format necessaire afin d'afficher vent, humidité et visibilité.
+curl -s --max-time 10 "wttr.in/${VILLE}?format=%t|%w|%h|%v&no-terminal" -o "$DATA"
+
+if [ ! -s "$DATA" ]; then
+    echo "Erreur : impossible de récupérer les données météo pour $VILLE."
+    exit 1
+fi
+
+RAW=$(cat "$DATA")
+# Supprimer séquences ANSI éventuelles
+RAW=$(echo "$RAW" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
+
+IFS='|' read -r TEMP VENT HUMIDITE VISIBILITE <<< "$RAW"
+
+
+TEMP="${TEMP#+}"
+
+# Nettoyer VENT (ne garder que les chiffres et km/h) afin d'éviter les problèmes d affichage
+VENT_CHIFFRES=$(echo "$VENT" | grep -o '[0-9]\+ *km/h')
+if [ -z "$VENT_CHIFFRES" ]; then
+    VENT_CHIFFRES="N/A"
+fi
+
+# Visibilité
+if [ -z "$VISIBILITE" ]; then
+    VISIBILITE="N/A"
+fi
+
+DATE=$(date +"%Y-%m-%d -%H:%M")
+
+echo "${DATE} -${VILLE} : ${TEMP}°C - Vent : ${VENT_CHIFFRES} - Humidite : ${HUMIDITE} - Visibilite : ${VISIBILITE}" >> "$METEO"
+
+rm "$DATA"
+
+
+#______________________________________________________Variante 2:________________________________________________________________________
+
+
+
+
+
+
+#______________________________________________________Variante 3:________________________________________________________________________
+
